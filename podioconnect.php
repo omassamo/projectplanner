d0786b2af509ed958a85216a816d806111331e7f
Matias Bruhn <matias.bruhn@bacondeczar.comgit config --global user.name Matias>
2014-05-21 16:14:23 +0200

    cleaning up

--- podioconnect.php
+++ podioconnect.php
@@ -5,11 +5,10 @@ $client_secret = "LX6k53Q8oFf1e7eEOzCJ2KtKgELXrTiccsdjJoth4hA8O6wKuEjh8O8zoXmppa
 $app_id = "6075847";
 $app_token = "8210c97b3ff4479cb60a5455469be5d2";
 
-
-echo('php');
-
+// Podio API
 require_once('podio-php-4.0.0/PodioAPI.php');
 
+// Authenticate with API
 Podio::setup($client_id, $client_secret);
 
 try {
@@ -27,11 +26,9 @@ catch (PodioError $e) {
   // Something went wrong. Examine $e->body['error_description'] for a description of the error.
 };
 
+// Retrieve data from app
+
 Podio::authenticate_with_app($app_id, $app_token);
-
-// $items = PodioItem::filter($app_id);
-
-// print("My app has ".count($items)." items");
 
 $allitems = PodioItem::filter($app_id);
 
